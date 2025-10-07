a6132abd15d2eaa914b2a9f493664d44bb54c406
Helen Yohanna Ventura Della Roveri <86626344+HelenDellaRoveri@users.noreply.github.com>
2025-10-07 14:11:49 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -10,5 +10,30 @@ class Task{
 }
 
 class TaskManager{
+    constructor(){
+        this.tasks = [];
+    }
+
+    addTask(description){
+        const task = new Task(description);
+        this.tasks.push(task);
+        this.displayTasks(); 
+    }
+
+    removeTask(index){
+        this.tasks.splice(index, 1);
+        this.displayTasks();
+    }
+
+    toggleTaskComplete(index){
+        this.tasks[index].toggleCompleted();
+        this.displayTasks();
+    }
+
+    displayTasks(){
+        const taskItem = document.createElement('li');
+        taskItem.className = task.completed ? 'completed' : '';
+        
     
+    }
 }
